c7c2055c9c4156734cbc25addb4d1b2bfc047cc0
Mateus Duarte <mateusrduarte@gmail.com>
2024-11-11 17:26:04 -0300

    Implementado o metodo limpar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+limpar();
+
 function adicionar() {
   let produto = document.getElementById("produto").value.split(" - ");
   let quantidade = document.getElementById("quantidade").value;
@@ -7,4 +9,10 @@ function adicionar() {
     ".carrinho__produtos__produto"
   ).innerHTML = `<span class="texto-azul">${quantidade}x</span> ${produto[0]} <span class="texto-azul">${produto[1]}</span>`;
   document.getElementById("valor-total").innerHTML = `RS${valor * quantidade}`;
+}
+
+function limpar() {
+  document.getElementById("quantidade").value = "";
+  document.querySelector(".carrinho__produtos__produto").innerHTML = "";
+  document.getElementById("valor-total").innerHTML = "R$0";
 }
